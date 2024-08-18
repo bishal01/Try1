5bd6bc676b73f01038c291e953f128eb34b3f2a6
bishal01 <51049096+bishal01@users.noreply.github.com>
2024-08-18 14:41:44 +0545

    hii

--- index.js
+++ index.js
@@ -1,57 +1,34 @@
-import express from 'express'
-import dotenv from 'dotenv';
+const express = require('express');
+const dotenv = require('dotenv');
 
 dotenv.config();
-const app = express()
-const port=process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.send('hello world')
-})
+  res.send('hello world');
+});
 
-app.get('/jokes',(req,res)=>{
-  const jokes=[
-    {
-      id:1,
-      name:"jokes1",
-      desc:"this is jokes 1"
-    },
-    {
-      id:2,
-      name:"jokes2",
-      desc:"this is jokes 2"
-    },
-    {
-      id:3,
-      name:"jokes3",
-      desc:"this is jokes 3"
-    },
-    {
-      id:4,
-      name:"jokes4",
-      desc:"this is jokes 4"
-    },
-  ]
-  res.send(jokes)
-})
+app.get('/jokes', (req, res) => {
+  const jokes = [
+    { id: 1, name: 'jokes1', desc: 'this is jokes 1' },
+    { id: 2, name: 'jokes2', desc: 'this is jokes 2' },
+    { id: 3, name: 'jokes3', desc: 'this is jokes 3' },
+    { id: 4, name: 'jokes4', desc: 'this is jokes 4' },
+  ];
+  res.send(jokes);
+});
 
 // GET method route
-app.get('/', (req, res) => {
-    res.send('GET request to the homepage')
-  })
-  
-app.get('/hello',(req,res)=>{
-res.send("Hii what's up")
+app.get('/hello', (req, res) => {
+  res.send("Hii what's up");
+});
 
-}  )
+app.get('/login', (req, res) => {
+  res.send('Login Page');
+});
 
-app.get('/login',(req,res)=>{
-    res.send("Login Page")
-})
-
-app.listen(port,()=>{
-    console.log(`Yoo i m listening in ${port}`)
-})
-
- 
+app.listen(port, () => {
+  console.log(`Yoo I’m listening on port ${port}`);
+});
