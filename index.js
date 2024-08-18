6bf97cd1d0b4ada4dfdb9bf78d76b563c04d784c
bishal01 <51049096+bishal01@users.noreply.github.com>
2024-08-18 13:52:04 +0545

    hii

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-require('dotenv').config()
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express()
-port=3000
+const port=process.env.port
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/jokes',(req,res)=>{
+  const jokes=[
+    {
+      id:1,
+      name:"jokes1",
+      desc:"this is jokes 1"
+    },
+    {
+      id:2,
+      name:"jokes2",
+      desc:"this is jokes 2"
+    },
+    {
+      id:3,
+      name:"jokes3",
+      desc:"this is jokes 3"
+    },
+    {
+      id:4,
+      name:"jokes4",
+      desc:"this is jokes 4"
+    },
+  ]
+  res.send(jokes)
 })
 
 // GET method route
@@ -17,6 +45,10 @@ app.get('/hello',(req,res)=>{
 res.send("Hii what's up")
 
 }  )
+
+app.get('/login',(req,res)=>{
+    res.send("Login Page")
+})
 
 app.listen(process.env.port,()=>{
     console.log(`Yoo i m listening in ${port}`)
